6558f963915b1c9260ddf16a6da7b172aa37f069
SrushtiRindhe09 <rindhesrushti2006@gmail.com>
2026-01-21 22:32:12 +0530

    done intial changes in files

--- ojt-management-frontend/src/App.jsx
+++ ojt-management-frontend/src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>OJT Management System</h1>
+    <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-4xl font-bold">
+        Tailwind is Working 🚀
+      </h1>
     </div>
   );
 }
